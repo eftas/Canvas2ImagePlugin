651cf649760003fcc3a680c93b26f82e69142500
Georg Kaspar <georg.kaspar@eftas.com>
2018-06-26 11:55:34 +0200

    added fixes for cordova-ios 4 from racekarl (26.06.2016)

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -4,11 +4,14 @@
 //
 //  Created by Tommy-Carlos Williams on 29/03/12.
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
-//	MIT Licensed
+//  MIT Licensed
 //
 
 #import "Canvas2ImagePlugin.h"
 #import <Cordova/CDV.h>
+#ifndef __CORDOVA_4_0_0
+    #import <Cordova/NSData+Base64.h>
+#endif
 
 @implementation Canvas2ImagePlugin
 @synthesize callbackId;
@@ -22,35 +25,50 @@
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
-	NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
+    //NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
+    
+    //NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0]];
+    
+#ifndef __CORDOVA_3_8_0
+    NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
+#else
+    //NSData* data = [[NSData alloc] initWithBase64EncodedString:dataString options:0];
+    NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
+#endif
+    
+    
+    UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
 }
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
 {
+    
+    CDVPluginResult* pluginResult = nil;
+    
     // Was there an error?
     if (error != NULL)
     {
         // Show error message...
         NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+        pluginResult= [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+        //[self.webView stringByEvaluatingJavaScriptFromString:[result toErrorCallbackString: self.callbackId]];
     }
     else  // No errors
     {
         // Show message image successfully saved
         NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
+        //[self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
     }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
 - (void)dealloc
-{	
-	[callbackId release];
+{   
+    [callbackId release];
     [super dealloc];
 }
 
